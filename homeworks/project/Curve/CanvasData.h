1d7768c9865644ecc29f76b64a16d0096facfc6a
hpy <657963486@qq.com>
2022-10-26 09:02:49 +0800

    bezier and uniform b-spline done

---
+++ homeworks/project/Curve/CanvasData.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include"imgui.h"
+#include <vector>
+using namespace std;
+
+struct CanvasData {
+	//面板控制
+	bool monitor_open = { false };
+	//控制点
+	vector<ImVec2> points;
+
+	//bezier
+	vector<ImVec2> bezier_curve;
+	int k = 3;
+	bool bezier_draw = { false };
+
+	//B-spline
+	vector<ImVec2> b_curve;
+	bool b_draw = { false };
+
+	//每段采样点数量
+	int num = 1000;
+
+	//修改
+	bool editing = { false };
+	bool is_draging = { false };
+	int edit_idx = { -1 };
+
+	void Reset() {
+		monitor_open = false;
+		points.clear();
+
+		bezier_curve.clear();
+		bezier_draw = false;
+
+		b_curve.clear();
+		b_draw = false;
+
+		editing = false;
+		is_draging = false;
+		edit_idx = -1;
+	}
+};
